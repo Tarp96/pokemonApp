f516bcf694a293412e9860a9ed549337cb7973cf
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-13 18:52:04 +0200

    Fixed routing issue for Details page and api data gets brought in properly.

--- src/pages/PokemonDetailsPage.jsx
+++ src/pages/PokemonDetailsPage.jsx
@@ -1,16 +1,29 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { fetchPokemonDetails } from "../utils/pokeApi";
+import { useParams } from "react-router-dom";
 
-export const PokemonDetailsPage = ({ name }) => {
+export const PokemonDetailsPage = () => {
+  const { name } = useParams();
+
   const [pokemon, setPokemon] = useState({});
   const [loading, setLoading] = useState(false);
 
-  useState(() => {
-    setLoading((prev) => (prev = true));
-    const pokemon = fetchPokemonDetails(name);
-    setPokemon(() => fetchPokemonDetails(name));
-    console.log(pokemon);
-  }, []);
+  useEffect(() => {
+    const loadPokemonDetails = async () => {
+      try {
+        setLoading(true);
+        const data = await fetchPokemonDetails(name);
+        setPokemon(data);
+        console.log(data);
+      } catch (error) {
+        console.error("Failed to fetch Pokémon details:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-  return <div>PokemonDetails</div>;
+    loadPokemonDetails();
+  }, [name]);
+
+  return <h1>{pokemon.name}</h1>;
 };
