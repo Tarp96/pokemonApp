c725c5e7823759d52a4da7654f49b77186599f9b
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-15 15:02:22 +0200

    Sectioned the JSX with div containers.

--- src/pages/PokemonDetailsPage.jsx
+++ src/pages/PokemonDetailsPage.jsx
@@ -42,42 +42,44 @@ export const PokemonDetailsPage = () => {
     <p>Loading...</p>
   ) : (
     <div className="detailsPageContainer">
-      <h1 className="detailsPageTitle">
-        {pokemon.name && firstLetterUpperCase(pokemon.name)} {`#${pokemon.id}`}
-      </h1>
-
-      <div className="imageAndStatsContainer">
-        {pokemon.sprites?.other["official-artwork"]?.front_default && (
-          <img
-            src={pokemon.sprites.other["official-artwork"].front_default}
-            alt="Official artwork"
-            className="mainDetailImage"
-          />
-        )}
-
+      <div className="detailsPageHeader">
+        <h1 className="detailsPageTitle">
+          {pokemon.name && firstLetterUpperCase(pokemon.name)} #{pokemon.id}
+        </h1>
+      </div>
+      <div className="detailsTopSection">
+        <div className="mainImageContainer">
+          {pokemon.sprites?.other["official-artwork"]?.front_default && (
+            <img
+              src={pokemon.sprites.other["official-artwork"].front_default}
+              alt="Official artwork"
+              className="mainDetailImage"
+            />
+          )}
+        </div>
         <div className="statsContainer">{stats}</div>
       </div>
 
-      <div>
-        <h2>Abilities:</h2>
-        {abilities}
+      <div className="detailsMiddleSection">
+        <div className="abilitiesContainer">
+          <h2>Abilities</h2>
+          {abilities}
+        </div>
+        <div className="typesContainer">
+          <h2>Types</h2>
+          {types}
+        </div>
       </div>
-      <div>
-        <h2>Types: </h2>
-        {types}
-      </div>
-      <div className="pokemonDetailPictureContainer">
+      <div className="spritesSection">
         {pokemon.sprites?.front_default && (
           <img src={pokemon.sprites.front_default} alt="Front default sprite" />
         )}
         {pokemon.sprites?.back_default && (
           <img src={pokemon.sprites.back_default} alt="Back default sprite" />
         )}
-
         {pokemon.sprites?.front_shiny && (
           <img src={pokemon.sprites.front_shiny} alt="Front shiny sprite" />
         )}
-
         {pokemon.sprites?.back_shiny && (
           <img src={pokemon.sprites.back_shiny} alt="Back shiny sprite" />
         )}
