e6e9523d592c7e55dfe154f735db68c54630c39d
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-19 15:41:54 +0200

    feat: Added section and image to photo page.
    Added the front default sprite to the photos page. Needs styling.

--- src/pages/PokemonDetailsPage/PokemonPhotos.jsx
+++ src/pages/PokemonDetailsPage/PokemonPhotos.jsx
@@ -1,9 +1,21 @@
 import { useOutletContext } from "react-router-dom";
 
 export const PokemonPhotos = () => {
+  const { pokemon } = useOutletContext();
+
   return (
     <div className="photosPageContainer">
-      <h1>Photos Page</h1>
+      <h1>Photos Page</h1> 
+      <section className="defaultImagesSectionContainer">
+        {pokemon.sprites?.front_default && (
+          <img
+            src={
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/382.png"
+            }
+            alt="Default front sprite"
+          />
+        )}
+      </section>
     </div>
   );
 };
