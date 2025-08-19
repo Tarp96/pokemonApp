2940663b703a04283ef3d52b0b5415d828de1644
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-19 15:53:07 +0200

    feat: Added a url extractor function.
    Added a url extractor function that will loop through the sprites object and get push the valid images into an array

--- src/pages/PokemonDetailsPage/PokemonPhotos.jsx
+++ src/pages/PokemonDetailsPage/PokemonPhotos.jsx
@@ -3,14 +3,40 @@ import { useOutletContext } from "react-router-dom";
 export const PokemonPhotos = () => {
   const { pokemon } = useOutletContext();
 
+  function extractImageUrls(spriteObj) {
+    const images = [];
+
+    function recurse(obj) {
+      for (const [key, value] of Object.entries(obj)) {
+        if (typeof value === "string" && value.startsWith("https://")) {
+          images.push({ key, url: value });
+        } else if (typeof value === "object" && value !== null) {
+          recurse(value);
+        }
+      }
+    }
+
+    recurse(spriteObj);
+    return images;
+  }
+
   return (
     <div className="photosPageContainer">
       <h1>Photos Page</h1> 
-      <section className="defaultImagesSectionContainer">
+      <section className="defaultSpritesSection">
         {pokemon.sprites?.front_default && (
           <img
             src={
               "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/382.png"
+            }
+            alt="Default front sprite"
+          />
+        )}
+
+        {pokemon.sprites?.front_shiny && (
+          <img
+            src={
+              "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/382.png"
             }
             alt="Default front sprite"
           />
