132b4732c812ca9eb8b0199a31f1c0bee66f6052
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-20 15:14:42 +0200

    feat: Move array gets displayed
    Added useEffect that populates the moves array on page load in. List is displayed in ul.

--- src/pages/PokemonDetailsPage/PokemonMoves.jsx
+++ src/pages/PokemonDetailsPage/PokemonMoves.jsx
@@ -1,9 +1,27 @@
 import { useOutletContext } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const PokemonMoves = () => {
+  const { pokemon } = useOutletContext();
+  const [moves, setMoves] = useState([]);
+
+  useEffect(() => {
+    populateMoveArr();
+  }, []);
+
+  function populateMoveArr() {
+    const emptyArr = [];
+    pokemon.moves?.map((m) => emptyArr.push(m.move.name));
+    setMoves(emptyArr);
+    console.log(emptyArr);
+  }
+
+  const displayMoves = moves.map((m) => <li>{m}</li>);
+
   return (
     <div>
       <h1>Pokemon Moves</h1>
+      <ul>{displayMoves}</ul>
     </div>
   );
 };
