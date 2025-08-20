77f3fdcf8ed8933fb86991943852d8b21e39d146
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-20 18:27:23 +0200

    feat: Added names and urls to gameImages array.

--- src/utils/gameImages.js
+++ src/utils/gameImages.js
@@ -1,1 +1,93 @@
-export const gameImages = [];
+export const gameImages = [
+  {
+    name: "red",
+    imgUrl: "https://m.media-amazon.com/images/I/71aow5iRsfL.jpg",
+  },
+  {
+    name: "blue",
+    imgUrl:
+      "https://m.media-amazon.com/images/I/71tFts8ENbL._UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "yellow",
+    imgUrl:
+      "https://assets1.ignimgs.com/2019/05/17/pokemon-yellow---button-1558057648010.jpg?crop=1%3A1%2Csmart&format=jpg&auto=webp&quality=80",
+  },
+  {
+    name: "gold",
+    imgUrl:
+      "https://m.media-amazon.com/images/I/61X24yubIHL._UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "silver",
+    imgUrl: "https://m.media-amazon.com/images/I/81UQlso+frL.jpg",
+  },
+  {
+    name: "crystal",
+    imgUrl: "https://m.media-amazon.com/images/I/510mrnxDhPL.jpg",
+  },
+  {
+    name: "ruby",
+    imgUrl: "https://m.media-amazon.com/images/I/81oHMwltcrL.jpg",
+  },
+  {
+    name: "sapphire",
+    imgUrl:
+      "https://upload.wikimedia.org/wikibooks/en/0/02/Pokemon_Sapphire.jpg?20210628191850",
+  },
+  {
+    name: "emerald",
+    imgUrl:
+      "https://upload.wikimedia.org/wikipedia/en/f/f7/PokemonEmeraldBox.jpg",
+  },
+  {
+    name: "firered",
+    imgUrl:
+      "https://images-cdn.ubuy.co.in/683c5be390920b72f30e9e06-pocket-gen-3-fire-red-version-gba-game.jpg",
+  },
+  {
+    name: "leafgreen",
+    imgUrl:
+      "https://cdn2.steamgriddb.com/thumb/4d44ee6d2168b32a35a2f3744332e64f.png",
+  },
+  {
+    name: "diamond",
+    imgUrl: "https://m.media-amazon.com/images/I/71lvijDJomL.jpg",
+  },
+  {
+    name: "pearl",
+    imgUrl: "https://m.media-amazon.com/images/I/61MM1UJWfeL.jpg",
+  },
+  {
+    name: "platinum",
+    imgUrl: "https://m.media-amazon.com/images/I/71vbqOrCCWL.jpg",
+  },
+  {
+    name: "heartgold",
+    imgUrl: "https://m.media-amazon.com/images/I/716CgyyiuCL.jpg",
+  },
+  {
+    name: "soulsilver",
+    imgUrl:
+      "https://m.media-amazon.com/images/I/71WlibEzVaL._UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "white",
+    imgUrl:
+      "https://m.media-amazon.com/images/I/81ZhVKZo2+L._UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "black",
+    imgUrl: "https://m.media-amazon.com/images/I/81mLFXTe3ES.jpg",
+  },
+  {
+    name: "white-2",
+    imgUrl:
+      "https://m.media-amazon.com/images/I/71N-UGMjFBL._UF1000,1000_QL80_.jpg",
+  },
+  {
+    name: "black-2",
+    imgUrl:
+      "https://m.media-amazon.com/images/I/815GvAb76VL._UF1000,1000_QL80_.jpg",
+  },
+];
