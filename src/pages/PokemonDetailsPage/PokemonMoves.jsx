016c5ebf150d0b43e7825fa60021c7b949679e0b
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-20 15:26:19 +0200

    feat: List displays alphabetically.
    List now displays alphabetically and the movenames start with uppercase with use of the firstLetterTouppercase func

--- src/pages/PokemonDetailsPage/PokemonMoves.jsx
+++ src/pages/PokemonDetailsPage/PokemonMoves.jsx
@@ -1,5 +1,6 @@
 import { useOutletContext } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { firstLetterUpperCase } from "../../utils/helperFunctions";
 
 export const PokemonMoves = () => {
   const { pokemon } = useOutletContext();
@@ -7,10 +8,13 @@ export const PokemonMoves = () => {
 
   useEffect(() => {
     const moveNames = pokemon.moves?.map((m) => m.move.name) || [];
+    moveNames.sort();
     setMoves(moveNames);
   }, [pokemon]);
 
-  const displayMoves = moves.map((m) => <li key={m}>{m}</li>);
+  const displayMoves = moves.map((m) => (
+    <li key={m}>{firstLetterUpperCase(m)}</li>
+  ));
 
   return (
     <div>
