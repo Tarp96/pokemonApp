d4289050c5612965e4536ed46f7b389b3d05e0a9
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-21 13:03:55 +0200

    feat: Images not display on page.

--- src/pages/PokemonDetailsPage/PokemonGames.jsx
+++ src/pages/PokemonDetailsPage/PokemonGames.jsx
@@ -4,23 +4,20 @@ import { gameImages } from "./../../utils/gameImages";
 export const PokemonGames = () => {
   const { pokemon } = useOutletContext();
 
-  const displayGames = pokemon.game_indices.map((g) => {
+  const displayGames = pokemon.game_indices?.map((g) => {
     const matchedImage = gameImages.find((img) => img.name === g.version.name);
+
     return (
       <div key={g.version.name}>
         <p>{g.version.name}</p>
-        {matchedImage && <img src={matchedImage.imgUrl} alt={g.version.name} />}
+        {matchedImage ? (
+          <img src={matchedImage.imgUrl} alt={g.version.name} />
+        ) : (
+          <p>Image not available</p>
+        )}
       </div>
     );
   });
 
-  return (
-    <div>
-      {matchedImage ? (
-        <img src={matchedImage.imgUrl} alt={game.version.name} />
-      ) : (
-        <p>No image found</p>
-      )}
-    </div>
-  );
+  return <div>{displayGames}</div>;
 };
