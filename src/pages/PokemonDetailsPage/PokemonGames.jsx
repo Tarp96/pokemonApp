d4b706fdaa7093b92e56357783c6b437f6d8be0f
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-21 13:00:06 +0200

    refactor: Removed redundant useEffect and useState

--- src/pages/PokemonDetailsPage/PokemonGames.jsx
+++ src/pages/PokemonDetailsPage/PokemonGames.jsx
@@ -1,19 +1,26 @@
-import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
 import { gameImages } from "./../../utils/gameImages";
 
 export const PokemonGames = () => {
   const { pokemon } = useOutletContext();
-  const [games, setGames] = useState([]);
 
-  useEffect(() => {
-    const pokemonGames = pokemon.game_indices.map((g) => g.version.name) || [];
-    setGames(games);
-  }, [pokemon]);
-
-  games.map((g) => {
-    const matchedImage = gameImages.find((img) => img.name === g);
+  const displayGames = pokemon.game_indices.map((g) => {
+    const matchedImage = gameImages.find((img) => img.name === g.version.name);
+    return (
+      <div key={g.version.name}>
+        <p>{g.version.name}</p>
+        {matchedImage && <img src={matchedImage.imgUrl} alt={g.version.name} />}
+      </div>
+    );
   });
 
-  return <div>{}</div>;
+  return (
+    <div>
+      {matchedImage ? (
+        <img src={matchedImage.imgUrl} alt={game.version.name} />
+      ) : (
+        <p>No image found</p>
+      )}
+    </div>
+  );
 };
