b631780aaa1d3981eac5ea3a3f74f181d3ecb03c
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-21 10:35:21 +0200

    refactor: Imported gameImages arr and created outlet const

--- src/pages/PokemonDetailsPage/PokemonGames.jsx
+++ src/pages/PokemonDetailsPage/PokemonGames.jsx
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
 import { useOutletContext } from "react-router-dom";
+import { gameImages } from "./../../utils/gameImages";
 
 export const PokemonGames = () => {
+  const { pokemon } = useOutletContext();
+
   return (
     <div>
       <h1>Pokemon Games Page</h1>
