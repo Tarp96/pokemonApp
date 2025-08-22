23b99e86579857851399941bf27636ae2336e834
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-22 13:51:18 +0200

    refactor: Changed information text in the NoPokemonMatchFilter component

--- src/components/NoPokemonMatchFilter.jsx
+++ src/components/NoPokemonMatchFilter.jsx
@@ -1,7 +1,11 @@
 export const NoPokemonMatchFilter = ({ onClick }) => {
   return (
     <div className="ifNoPokemonMatchFilter">
-      <p>None of the Pokemon on current page belong to this type</p>
+      <p>
+        No matching Pokémon found for your current search or type filter. 🧭
+        <br />
+        Try a different name or choose another type!
+      </p>
       <button onClick={onClick}>Remove Filter</button>
       <img
         src="assets/pikaconfused.gif"
