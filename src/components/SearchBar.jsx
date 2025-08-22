5491e622fa7241ba2795083132b7072dfdd167c2
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-22 13:46:44 +0200

    style: Added transparet background to the searchbar for better visibility

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,17 @@
 export const SearchBar = ({ query, setQuery, onClick }) => {
   return (
-    <div className="searchbarContainer">
-      <input
-        type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        placeholder="Example: Charizard or Latios"
-      />
-      <button onClick={onClick} className="searchBarButton">
-        Search 🔎
-      </button>
+    <div className="searchBarContainer">
+      <div className="searchBarDiv">
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Example: Charizard or Latios"
+        />
+        <button onClick={onClick} className="searchBarButton">
+          Search 🔎
+        </button>
+      </div>
     </div>
   );
 };
