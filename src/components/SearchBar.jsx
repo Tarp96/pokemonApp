63c7c40fb6006ada67af88b2358ea7695cb4a760
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-22 13:14:42 +0200

    refactor: Changed placeholder text for searchbar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -5,7 +5,7 @@ export const SearchBar = ({ query, setQuery, onClick }) => {
         type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        placeholder="Example: Charizard or water 🔎"
+        placeholder="Example: Charizard or Latios 🔎"
       />
       <button onClick={onClick}>Search 🔎</button>
     </div>
