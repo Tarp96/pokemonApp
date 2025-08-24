8f90ff551828ca515882839f17b894d0de550ae4
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-24 12:07:03 +0200

    import: Imported Outlet from react-router-dom

--- src/pages/HomePageLayout.jsx
+++ src/pages/HomePageLayout.jsx
@@ -1,9 +1,11 @@
 import { Header } from "../components/Header";
+import { Outlet } from "react-router-dom";
 
 export const HomePageLayout = () => {
   return (
     <>
       <Header />
+      <Outlet />
     </>
   );
 };
