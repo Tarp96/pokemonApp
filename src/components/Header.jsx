26c447016ae9bbb11aee64fd39893165fd02e964
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-24 11:51:15 +0200

    remove: Removed Outlet import.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import { Outlet } from "react-router-dom";
-
 export const Header = () => {
   return (
-    <header>
-      <div>
-        <h1>Header</h1>
-      </div>
-    </header>
+    <>
+      <header>
+        <div>
+          <h1>Header</h1>
+        </div>
+      </header>
+    </>
   );
 };
