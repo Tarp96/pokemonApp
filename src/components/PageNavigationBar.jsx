99e49c596642f57bf4a7918fe6b2d3de20f51046
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-25 17:04:36 +0200

    refactor: Added div with className for styling

--- src/components/PageNavigationBar.jsx
+++ src/components/PageNavigationBar.jsx
@@ -1,3 +1,13 @@
-export const PageNavigationBar = ({ link }) => {
-  return <></>;
+import { NavLink } from "react-router-dom";
+
+export const PageNavigationBar = ({ links }) => {
+  return (
+    <div className="detailsNavigationBar">
+      {links.map(({ path, label }) => (
+        <NavLink key={path} to={path} end>
+          {label}
+        </NavLink>
+      ))}
+    </div>
+  );
 };
