f64329e08e4dcbe1590ca50b651acf5e548be601
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-25 18:26:24 +0200

    feat: Imported Outlet to ProfilePageOverview and added it below the header section

--- src/pages/ProfilePage/ProfilePageOverview.jsx
+++ src/pages/ProfilePage/ProfilePageOverview.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from "react-router-dom";
+
 export const ProfilePageOverview = () => {
   return (
     <div>
@@ -5,6 +7,8 @@ export const ProfilePageOverview = () => {
         <h1 className="profileMainTitle">Trainer Red</h1>
         <p className="profileSubtitle">Welcome back, Trainer!</p>
       </section>
+
+      <Outlet />
     </div>
   );
 };
