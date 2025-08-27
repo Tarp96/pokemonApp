ce159ab4aea7d39ef0727246b724712a900cbdd9
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-27 09:58:30 +0200

    feat: Favorites page now pulls array from local storage and displays item if array is not empty

--- src/pages/ProfilePage/FavoritePokemonsPage.jsx
+++ src/pages/ProfilePage/FavoritePokemonsPage.jsx
@@ -1,11 +1,20 @@
 import { useEffect, useState } from "react";
+import { getItem } from "../../utils/localStorage";
 
 export const FavoritePokemonsPage = () => {
-  const [favoritePokemon, setFavoritePokemon] = useState(() => {});
+  const [favoritePokemon, setFavoritePokemon] = useState(() => {
+    const item = getItem("favorites");
+    return item || [];
+  });
+
+  const displayFavorites = favoritePokemon.map((pokemon) => (
+    <p>{pokemon.name}</p>
+  ));
 
   return (
     <div>
       <h1>Your Favorite Pokemon</h1>
+      {displayFavorites}
     </div>
   );
 };
