cbf1898149ff4502ee1133f2345221b821818601
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-27 09:52:36 +0200

    state: added favoritePokemon state to FavoritePokemonPage. Currently empty

--- src/pages/ProfilePage/FavoritePokemonsPage.jsx
+++ src/pages/ProfilePage/FavoritePokemonsPage.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 
 export const FavoritePokemonsPage = () => {
+  const [favoritePokemon, setFavoritePokemon] = useState(() => {});
+
   return (
     <div>
       <h1>Your Favorite Pokemon</h1>
