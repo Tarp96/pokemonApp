a9cb5e481bd77bce429aa568a77beb8512792095
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-28 12:55:18 +0200

    refactor: imported useState to FavoriteButton and created a clicked state

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -1,6 +1,9 @@
 import { TbStar, TbStarFilled } from "react-icons/tb";
+import { useState } from "react";
 
 export const FavoriteButton = ({ onClick, isClicked }) => {
+  const [clicked, setClicked] = useState(false);
+
   return (
     <div>
       <button onClick={onClick}>
