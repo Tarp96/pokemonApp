dec43c3f9de1fcf34177b018d4d7810b03edc9b1
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-28 15:12:01 +0200

    refactor: Wrapped Routes with fragment

--- src/App.jsx
+++ src/App.jsx
@@ -16,23 +16,25 @@ import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePageLayout />}>
-        <Route index element={<HomePage />} />
-        <Route path="profilepage" element={<ProfilePageOverview />}>
-          <Route index element={<ProfilePage />} />
-          <Route path="favorites" element={<FavoritePokemonsPage />} />
+    <>
+      <Routes>
+        <Route path="/" element={<HomePageLayout />}>
+          <Route index element={<HomePage />} />
+          <Route path="profilepage" element={<ProfilePageOverview />}>
+            <Route index element={<ProfilePage />} />
+            <Route path="favorites" element={<FavoritePokemonsPage />} />
+          </Route>
         </Route>
-      </Route>
 
-      <Route path="/pokemon/:name" element={<PokemonDetailsPage />}>
-        <Route index element={<PokemonDetailOverView />} />
-        <Route path="stats" element={<PokemonStats />} />
-        <Route path="photos" element={<PokemonPhotos />} />
-        <Route path="games" element={<PokemonGames />} />
-        <Route path="moves" element={<PokemonMoves />} />
-      </Route>
-    </Routes>
+        <Route path="/pokemon/:name" element={<PokemonDetailsPage />}>
+          <Route index element={<PokemonDetailOverView />} />
+          <Route path="stats" element={<PokemonStats />} />
+          <Route path="photos" element={<PokemonPhotos />} />
+          <Route path="games" element={<PokemonGames />} />
+          <Route path="moves" element={<PokemonMoves />} />
+        </Route>
+      </Routes>
+    </>
   );
 }
 
