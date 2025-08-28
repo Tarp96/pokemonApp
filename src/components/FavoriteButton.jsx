fe46732cdcdcf6f1eaf22ba20701f62d98a2aec9
Tarpi96 <tarpisingh.96@gmail.com>
2025-08-28 13:03:14 +0200

    refactor: removed redundant state in FavoriteButton

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -1,9 +1,6 @@
 import { TbStar, TbStarFilled } from "react-icons/tb";
-import { useState } from "react";
 
 export const FavoriteButton = ({ onClick, isClicked }) => {
-  const [clicked, setClicked] = useState(false);
-
   return (
     <div>
       <button onClick={onClick}>
