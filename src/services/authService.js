2b2314113adbdb0603fc1452b36b0055218efd21
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-01 15:09:05 +0200

    feat: Created function for user to sign in with email and password

--- src/services/authService.js
+++ src/services/authService.js
@@ -10,6 +10,10 @@ export const doCreateUserWithEmailAndPassword = async (email, password) => {
   return createUserWithEmailAndPassword(auth, email, password);
 };
 
+export const doSignInWithEmailAndPassword = (email, password) => {
+  return signInWithEmailAndPassword(auth, email, password);
+};
+
 export const doSignInWithGoogle = async () => {
   const provider = new GoogleAuthProvider();
   const result = await signInWithPopup(auth, provider);
