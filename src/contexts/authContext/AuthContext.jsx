7a7fbedc503a48fbcda0a7cbcef23e298e2dec9e
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-01 14:09:41 +0200

    feat: Created useAuth hook

--- src/contexts/authContext/AuthContext.jsx
+++ src/contexts/authContext/AuthContext.jsx
@@ -1,8 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { auth } from "../../firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
 
 const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
 
 export function AuthProvider({ children }) {
   const [currentUser, setCurrentUser] = useState(null);
