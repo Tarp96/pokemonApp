615be55b0cf768d53e494ab1e0a3e22ad274ef8c
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-01 15:05:00 +0200

    feat: Created function for user to sign in with google account

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,9 +1,16 @@
+import { GoogleAuthProvider } from "firebase/auth/web-extension";
 import { auth } from "../firebaseConfig";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
+  signInWithPopup,
 } from "firebase/auth";
 
 export const doCreateUserWithEmailAndPassword = async (email, password) => {
-  return createUserWithEmailAndPassword(email, password);
+  return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const doSignInWithGoogle = async () => {
+  const provider = new GoogleAuthProvider();
+  const result = await signInWithPopup(auth, provider);
 };
