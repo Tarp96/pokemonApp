0d02c8e0f8075040fbb36ccd704490633603d715
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-01 16:27:31 +0200

    import: Imported state, auth function and pokeball logo to RegisterUserPage.jsx

---
+++ src/pages/Auth/RegisterUserPage.jsx
@@ -0,0 +1,4 @@
+import { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebaseConfig";
+import pokeb from "../../../public/assets/pokeb.png";
