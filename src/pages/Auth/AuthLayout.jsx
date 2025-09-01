ebadcd388809675f020cf4b421f98daa778cb240
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-01 18:13:28 +0200

    refactor: Added div and classname to Aut layout for styling

--- src/pages/Auth/AuthLayout.jsx
+++ src/pages/Auth/AuthLayout.jsx
@@ -2,9 +2,9 @@ import { Outlet } from "react-router-dom";
 
 export const AuthLayout = () => {
   return (
-    <>
+    <div className="authLayoutContainer">
       <h2>Log in or register!</h2>
       <Outlet />
-    </>
+    </div>
   );
 };
