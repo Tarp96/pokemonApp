b9c254a3125db9440808f416945522628c4fcd72
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-03 16:39:15 +0200

    feat: ProfilePageOverview displays username from firebase and passes it to children through otulet context

--- src/pages/ProfilePage/ProfilePageOverview.jsx
+++ src/pages/ProfilePage/ProfilePageOverview.jsx
@@ -1,14 +1,42 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useOutletContext } from "react-router-dom";
 import { PageNavigationBar } from "../../components/PageNavigationbar";
 import { useEffect, useState } from "react";
 import { doc, getDoc } from "firebase/firestore";
 import { db, auth } from "../../firebaseConfig";
+import { firstLetterUpperCase } from "../../utils/helperFunctions";
 
 export const ProfilePageOverview = () => {
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const fetchUsername = async () => {
+      const user = auth.currentUser;
+      if (!user) return;
+
+      try {
+        const docRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(docRef);
+
+        if (docSnap.exists()) {
+          const data = docSnap.data();
+          setUsername(data.username);
+        } else {
+          console.warn("No profile found for this user.");
+        }
+      } catch (err) {
+        console.error("Error fetching username:", err);
+      }
+    };
+
+    fetchUsername();
+  }, []);
+
   return (
     <div className="profilePageContainer">
       <section className="profilePageTitleSection">
-        <h1 className="profileMainTitle">Trainer Red</h1>
+        <h1 className="profileMainTitle">
+          {firstLetterUpperCase(username) || "Trainer"}
+        </h1>
         <p className="profileSubtitle">Welcome back, Trainer!</p>
       </section>
 
@@ -21,7 +49,7 @@ export const ProfilePageOverview = () => {
         />
       </div>
 
-      <Outlet />
+      <Outlet context={{ username }} />
     </div>
   );
 };
