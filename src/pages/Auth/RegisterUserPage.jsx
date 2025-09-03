00f74a88448836f01dcab91acca1e4f9552438a6
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-03 15:51:14 +0200

    feat: Added an input field for username

--- src/pages/Auth/RegisterUserPage.jsx
+++ src/pages/Auth/RegisterUserPage.jsx
@@ -54,6 +54,15 @@ const RegisterUserPage = () => {
           <h2 className="registerTitle">Join the Adventure!</h2>
 
           <form onSubmit={handleRegister} className="registerForm">
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              required
+              value={userName}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+
             <label htmlFor="email">Email</label>
             <input
               id="email"
