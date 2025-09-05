768be7484bea2d1512a962d9fcb5beaa4a3c7dd2
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-05 11:17:33 +0200

    feat: Createt function to get cache items

--- src/utils/cache.js
+++ src/utils/cache.js
@@ -4,3 +4,12 @@ export function cacheSet(key, value) {
   const payload = { t: Date.now(), v: value };
   setItem(key, payload);
 }
+
+export function cacheGet(key, maxAgeMs) {
+  const payload = getItem(key);
+  if (!payload || typeof payload !== "object") return null;
+  const { t, v } = payload;
+  if (typeof t !== "number") return null;
+  if (Date.now() - t > maxAgeMs) return null;
+  return v;
+}
