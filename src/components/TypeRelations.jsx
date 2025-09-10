b36c0ce4f6b0a5a4cbe27ff2b94edcd86672da49
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-10 13:35:24 +0200

    feat(typerelations): Created all types array

--- src/components/TypeRelations.jsx
+++ src/components/TypeRelations.jsx
@@ -1,1 +1,22 @@
 import { useEffect, useMemo, useState } from "react";
+
+const ALL_TYPES = [
+  "normal",
+  "fire",
+  "water",
+  "electric",
+  "grass",
+  "ice",
+  "fighting",
+  "poison",
+  "ground",
+  "flying",
+  "psychic",
+  "bug",
+  "rock",
+  "ghost",
+  "dragon",
+  "dark",
+  "steel",
+  "fairy",
+];
