d62769a1677e3c28a51708774e61934c109a5d95
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-11 16:13:01 +0200

    refactor(SwitchButton): SwitchButton component takes classname prop;

--- src/components/SwitchButton.jsx
+++ src/components/SwitchButton.jsx
@@ -1,14 +1,20 @@
-export const SwitchButton = ({ onClick, condition, firstText, secondText }) => {
+export const SwitchButton = ({
+  onClick,
+  condition,
+  firstText,
+  secondText,
+  className = "",
+}) => {
   return (
     <>
       <button
-        className={`tabButton ${!condition ? "active" : ""}`}
+        className={`tabButton ${!condition ? "active" : ""} ${className}`}
         onClick={onClick}
       >
         {firstText}
       </button>
       <button
-        className={`tabButton ${condition ? "active" : ""}`}
+        className={`tabButton ${condition ? "active" : ""} ${className}`}
         onClick={onClick}
       >
         {secondText}
