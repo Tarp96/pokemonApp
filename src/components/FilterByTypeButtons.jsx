7a43bbf569dd8417cc6c63797a46ed5b5020b812
Tarpi96 <tarpisingh.96@gmail.com>
2025-09-17 15:16:33 +0200

    style: Filter buttons display type icon; Reduced width of filter button container;

--- src/components/FilterByTypeButtons.jsx
+++ src/components/FilterByTypeButtons.jsx
@@ -1,5 +1,6 @@
 import typeColors from "./../utils/typecolors";
 import { firstLetterUpperCase } from "../utils/helperFunctions";
+import { getTypeIcon } from "../utils/typeIcons";
 
 export const FilterByTypeButtons = ({ filterByTypeFunc, activeFilter }) => {
   const myObj = typeColors;
@@ -8,19 +9,21 @@ export const FilterByTypeButtons = ({ filterByTypeFunc, activeFilter }) => {
     <div className="filterButtonContainer">
       {Object.keys(myObj).map((key) => {
         const isActive = activeFilter === key;
+        const iconUrl = getTypeIcon(key);
 
         return (
           <button
             key={key}
+            className={isActive ? "active" : ""}
             style={{
-              border: `solid 2px ${myObj[key]}`,
+              border: `2px solid ${myObj[key]}`,
               backgroundColor: isActive ? myObj[key] : "white",
               color: isActive ? "white" : "black",
-              marginRight: "5px",
             }}
             onClick={() => filterByTypeFunc(key)}
           >
-            {firstLetterUpperCase(key)}
+            <span>{firstLetterUpperCase(key)}</span>
+            <img src={iconUrl} alt={`${key} icon`} />
           </button>
         );
       })}
