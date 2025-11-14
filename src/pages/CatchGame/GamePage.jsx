25bd2db4216c0f80c9bd680b3516dc6a508bbd1a
Tarpi96 <tarpisingh.96@gmail.com>
2025-11-14 12:09:04 +0100

    feat: added div for difficulty option buttons

--- src/pages/CatchGame/GamePage.jsx
+++ src/pages/CatchGame/GamePage.jsx
@@ -9,6 +9,13 @@ export const GamePage = () => {
           <h1>Ready to play?</h1>
           <p>Catch the pokemon that appears to earn coins</p>
         </div>
+        <div className="difficultyButtonContainer">
+          <h2>Choose difficulty</h2>
+          <p>Higher dificulty gives more coins</p>
+          <button>Easy</button>
+          <button>Medium</button>
+          <button>Hard</button>
+        </div>
         <div>
           <button className="gameStartButton">Start</button>
         </div>
