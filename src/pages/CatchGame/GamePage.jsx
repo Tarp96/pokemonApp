25d9bb947c42f80f35ebe07eef44844393500c59
Tarpi96 <tarpisingh.96@gmail.com>
2025-11-24 16:16:48 +0100

    refactor: Extracted game start screen to seperate component;

--- src/pages/CatchGame/GamePage.jsx
+++ src/pages/CatchGame/GamePage.jsx
@@ -1,58 +1,18 @@
 import { useState, useEffect } from "react";
+import { GameStartScreen } from "./GameStartScreen";
+import { GamePlayScreen } from "./GamePlayScreen";
 
 export const GamePage = () => {
-  const [difficulty, setDifficulty] = useState("Easy");
-  const [gameStareted, setGameStarted] = useState(false);
+  const [difficulty, setDifficulty] = useState(null);
+  const [gameStarted, setGameStarted] = useState(false);
 
-  useEffect(() => {
-    decideDifficulty("Easy");
-  }, []);
-
-  function decideDifficulty(difficulty) {
-    setDifficulty(difficulty);
-  }
-
-  function startGame() {
-    setGameStarted(true);
-  }
-
-  return (
-    <>
-      <div className="gameStartPageContainer">
-        <div className="gamePageContent">
-          <h1>Ready to play?</h1>
-          <p>Catch the pokemon that appears to earn coins</p>
-        </div>
-        <div className="difficultyButtonContainer">
-          <h2>Choose difficulty</h2>
-          <p>Higher dificulty gives more coins</p>
-          <p>Selected Difficulty: {difficulty}</p>
-          <button
-            onClick={() => decideDifficulty("Easy")}
-            className="difficultyBtn easy"
-          >
-            Easy
-          </button>
-          <button
-            onClick={() => decideDifficulty("Medium")}
-            className="difficultyBtn medium"
-          >
-            Medium
-          </button>
-          <button
-            onClick={() => decideDifficulty("Hard")}
-            className="difficultyBtn hard"
-          >
-            Hard
-          </button>
-        </div>
-        <div className="gameStartButtonContainer">
-          <button className="gameStartButton" onClick={startGame}>
-            Start
-            <img src="assets/pokeb.png" alt="" />
-          </button>
-        </div>
-      </div>
-    </>
+  return !gameStarted ? (
+    <GameStartScreen
+      selectedDifficulty={difficulty}
+      onDifficultyChange={setDifficulty}
+      onStart={() => setGameStarted(true)}
+    />
+  ) : (
+    <GamePlayScreen difficulty={difficulty} />
   );
 };
--- src/pages/CatchGame/GameStartScreen.jsx
+++ src/pages/CatchGame/GameStartScreen.jsx
@@ -1,1 +1,56 @@
-export const GameStartScreen = ({ selectedDifficulty }) => {};
+export const GameStartScreen = ({
+  selectedDifficulty,
+  onDifficultyChange,
+  onStart,
+}) => {
+  return (
+    <div className="gameStartPageContainer">
+      <div className="gamePageContent">
+        <h1>Ready to play?</h1>
+        <p>Catch the pokemon that appears to earn coins</p>
+      </div>
+
+      <div className="difficultyButtonContainer">
+        <h2>Choose difficulty</h2>
+        <p>Higher difficulty gives more coins</p>
+
+        <p>
+          Selected Difficulty:{" "}
+          <strong>{selectedDifficulty || "None selected"}</strong>
+        </p>
+
+        <button
+          onClick={() => onDifficultyChange("easy")}
+          className="difficultyBtn easy"
+        >
+          Easy
+        </button>
+
+        <button
+          onClick={() => onDifficultyChange("medium")}
+          className="difficultyBtn medium"
+        >
+          Medium
+        </button>
+
+        <button
+          onClick={() => onDifficultyChange("hard")}
+          className="difficultyBtn hard"
+        >
+          Hard
+        </button>
+      </div>
+
+      <div className="gameStartButtonContainer">
+        <button
+          className="gameStartButton"
+          onClick={onStart}
+          disabled={!selectedDifficulty}
+        >
+          Start
+          <img src="assets/pokeb.png" alt="pokeball icon" />
+        </button>
+      </div>
+    </div>
+  );
+};
