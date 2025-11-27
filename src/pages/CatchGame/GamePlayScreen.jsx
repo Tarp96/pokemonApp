1a3b219b5bcd2754bca8878fd5287c548d50280b
Tarpi96 <tarpisingh.96@gmail.com>
2025-11-27 11:03:27 +0100

    style: Wrapped gameplay screen in a container div; Added gengar image to asset folder;

--- src/pages/CatchGame/GamePlayScreen.jsx
+++ src/pages/CatchGame/GamePlayScreen.jsx
@@ -1,11 +1,11 @@
 export const GamePlayScreen = () => {
   return (
-    <>
+    <div>
       <div className="gameStartPageContainer">
         <div className="gamePageContent">
           <h1>Now Playing Game</h1>
         </div>
       </div>
-    </>
+    </div>
   );
 };
