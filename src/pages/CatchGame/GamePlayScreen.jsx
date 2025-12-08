7417cdac45a49288f2c3cc5911f4ea26c5ef11e4
Tarpi96 <tarpisingh.96@gmail.com>
2025-12-08 15:37:17 +0100

    feat: Gengar image appears in random locations 5 times

--- src/pages/CatchGame/GamePlayScreen.jsx
+++ src/pages/CatchGame/GamePlayScreen.jsx
@@ -1,13 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const GamePlayScreen = () => {
   const [position, setPosition] = useState({ top: 100, left: 100 });
+  const [count, setCount] = useState(5);
+
+  const generateRandomPosition = () => {
+    const maxWidth = 700;
+    const maxHeight = 800;
+
+    return {
+      top: Math.floor(Math.random() * maxHeight),
+      left: Math.floor(Math.random() * maxWidth),
+    };
+  };
+
+  useEffect(() => {
+    setPosition(generateRandomPosition());
+
+    const interval = setInterval(() => {
+      setCount((prev) => {
+        if (prev > 1) {
+          setPosition(generateRandomPosition());
+          return prev - 1;
+        } else {
+          clearInterval(interval);
+          return 0;
+        }
+      });
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div>
       <div className="gameStartPageContainer">
-        <div className="gamePageContent">
-          <h1>Now Playing Game</h1>
+        <div className="gamePageContent">{count}</div>
+        <div
+          className="targetItem"
+          style={{
+            top: position.top,
+            left: position.left,
+          }}
+        >
+          <img src="/assets/gengar.png" alt="Gengar" className="gamePokemon" />
         </div>
       </div>
     </div>
