92f7ed4723e711376445964f3fb18074fa9a0728
Tarpi96 <tarpisingh.96@gmail.com>
2026-01-23 16:15:07 +0100

    Moved game over related Jsx from GamePlayScreen to GameoverScreen

--- src/pages/CatchGame/GameOverScreen.jsx
+++ src/pages/CatchGame/GameOverScreen.jsx
@@ -1,1 +1,42 @@
-export const GameOverScreen = () => {};
+export const GameOverScreen = ({
+  score,
+  difficulty,
+  multiplier,
+  coinsEarned,
+}) => {
+  return (
+    <div className="gameOverCard">
+      <img
+        src="assets/pokemonTrophy.png"
+        alt="Person holding a big trophy"
+        className="gameOverPicture"
+      />
+
+      <h2 className="gameOverTitle">🎮 Game Over</h2>
+
+      <div className="gameOverStats">
+        <div className="statRow">
+          <span className="label">Score</span>
+          <span className="value">{score}</span>
+        </div>
+
+        <div className="statRow">
+          <span className="label">Difficulty</span>
+          <span className={`value difficulty ${difficulty.toLowerCase()}`}>
+            {difficulty}
+          </span>
+        </div>
+
+        <div className="statRow">
+          <span className="label">Multiplier</span>
+          <span className="value">x{coinMultiplier}</span>
+        </div>
+
+        <div className="statRow highlight">
+          <span className="label">Coins Earned</span>
+          <span className="value coins">💰 {coinsEarned}</span>
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/CatchGame/GamePlayScreen.jsx
+++ src/pages/CatchGame/GamePlayScreen.jsx
@@ -116,43 +116,7 @@ export const GamePlayScreen = ({ difficulty }) => {
   if (gameOver) {
     return (
       <>
-        <div className="gameOverPageContainer">
-          <div className="gameOverCard">
-            <img
-              src="assets/pokemonTrophy.png"
-              alt="Person holding a big trophy"
-              className="gameOverPicture"
-            />
-
-            <h2 className="gameOverTitle">🎮 Game Over</h2>
-
-            <div className="gameOverStats">
-              <div className="statRow">
-                <span className="label">Score</span>
-                <span className="value">{score}</span>
-              </div>
-
-              <div className="statRow">
-                <span className="label">Difficulty</span>
-                <span
-                  className={`value difficulty ${difficulty.toLowerCase()}`}
-                >
-                  {difficulty}
-                </span>
-              </div>
-
-              <div className="statRow">
-                <span className="label">Multiplier</span>
-                <span className="value">x{coinMultiplier}</span>
-              </div>
-
-              <div className="statRow highlight">
-                <span className="label">Coins Earned</span>
-                <span className="value coins">💰 {coinsEarned}</span>
-              </div>
-            </div>
-          </div>
-        </div>
+        <div className="gameOverPageContainer"></div>
       </>
     );
   }
