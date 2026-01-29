f74d8c6ebc14e4366b94b5f35f7258e50ece5c5f
Tarpi96 <tarpisingh.96@gmail.com>
2026-01-29 15:04:36 +0100

    coinService: Created function to get user coins from database

--- src/services/coinService.js
+++ src/services/coinService.js
@@ -6,3 +6,12 @@ import {
   increment,
   onSnapshot,
 } from "firebase/firestore";
+
+export const getUserCoins = async (uid) => {
+  const userRef = doc(db, "users", uid);
+  const snapshot = await getDoc(userRef);
+
+  if (!snapshot.exists()) return 0;
+
+  return snapshot.data().coins ?? 0;
+};
