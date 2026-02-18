7cbfe0204cf75167d4d7df7ce45112cecc5809ae
Tarpi96 <tarpisingh.96@gmail.com>
2026-02-18 12:15:44 +0100

    Imported auth and db from firebase config to PaymentModal; Imported listenToCoins from coinService to display users coin total and potential changes in coin total if purchase is made

--- src/components/PaymentModal.jsx
+++ src/components/PaymentModal.jsx
@@ -1,18 +1,31 @@
 import { getPokemonPrice } from "../data/pokemonPricing";
 import { useState, useEffect } from "react";
+import { db, auth } from "../firebaseConfig";
+import { listenToCoins } from "../services/coinService";
 
 export const PaymentModal = ({
   pokemon,
   purchaseOnClick,
   closeModalOnClick,
 }) => {
+  const [coinBalance, setCoinBalance] = useState();
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    if (!user) return;
+
+    const unsubscribe = listenToCoins(user.uid, setCoinBalance);
+
+    return () => unsubscribe();
+  }, []);
+
   const price = pokemon?.name ? getPokemonPrice(pokemon?.name) : 0;
 
   return (
     <div className="paymentModalOverlay">
       <div className="paymentModalContainer modalPop">
         <h2>Buy {pokemon?.name}?</h2>
-        <p>{price}</p>
+        <p>Price: {price}</p>
 
         <button onClick={purchaseOnClick}>Buy</button>
         <button onClick={closeModalOnClick}>Cancel</button>
