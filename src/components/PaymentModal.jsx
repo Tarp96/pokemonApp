5901c7f3b0439e559ac72bf7a7eb5da9775f4943
Tarpi96 <tarpisingh.96@gmail.com>
2026-02-18 12:53:48 +0100

    PaymentModal now displays coin balance and coin balance after purchase

--- src/components/PaymentModal.jsx
+++ src/components/PaymentModal.jsx
@@ -21,11 +21,19 @@ export const PaymentModal = ({
 
   const price = pokemon?.name ? getPokemonPrice(pokemon?.name) : 0;
 
+  function returnCoinTotalAfterPurchase(a, b) {
+    return a - b;
+  }
+
   return (
     <div className="paymentModalOverlay">
       <div className="paymentModalContainer modalPop">
         <h2>Buy {pokemon?.name}?</h2>
+        <p>Your coin balance: {coinBalance}</p>
         <p>Price: {price}</p>
+        <p>
+          Remaining coins: {returnCoinTotalAfterPurchase(coinBalance, price)}
+        </p>
 
         <button onClick={purchaseOnClick}>Buy</button>
         <button onClick={closeModalOnClick}>Cancel</button>
