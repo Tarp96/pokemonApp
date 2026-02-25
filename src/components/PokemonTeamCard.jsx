1152e71d827efdf2cbacf0a8676de6a1b235f9e3
Tarpi96 <tarpisingh.96@gmail.com>
2026-02-25 15:22:08 +0100

    Changed Team card to display more information and added classnames for styling

--- src/components/PokemonTeamCard.jsx
+++ src/components/PokemonTeamCard.jsx
@@ -1,7 +1,29 @@
+import { firstLetterUpperCase } from "../utils/helperFunctions";
+import { TypeBadge } from "./TypeBadge";
+
 export const PokemonTeamCard = ({ pokemon }) => {
+  const renderTypes = pokemon.types?.map((typeObj, index) => {
+    const typeName =
+      typeof typeObj === "string" ? typeObj : typeObj?.type?.name;
+
+    return <TypeBadge key={index} type={typeName} />;
+  });
+
   return (
-    <>
-      <p>{pokemon.name}</p>
-    </>
+    <div className="pokemonTeamCard">
+      <div className="teamCardSpriteWrapper">
+        <img
+          src={pokemon.sprite}
+          alt={pokemon.name}
+          className="teamCardSprite"
+        />
+      </div>
+
+      <div className="teamCardInfo">
+        <h3>{firstLetterUpperCase(pokemon.name)}</h3>
+
+        <div className="teamCardTypes">{renderTypes}</div>
+      </div>
+    </div>
   );
 };
