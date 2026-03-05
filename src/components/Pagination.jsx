c32494f79e0c6931d811962b5f334b10dbff992d
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-05 13:13:03 +0100

    Changed maxPagesToShow variable to a state to decrease number of buttons displayed on mobile screen

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,10 +1,27 @@
+import { useState, useEffect } from "react";
+
 const Pagination = ({ currentPage, totalPages, onPageChange }) => {
   const pageButtons = [];
-  const maxPagesToShow = 5;
+  const [maxPagesToShow, setMaxPagesToShow] = useState(5);
   const half = Math.floor(maxPagesToShow / 2);
 
   let startPage = Math.max(1, currentPage - half);
   let endPage = Math.min(totalPages, currentPage + half);
+
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth <= 480) {
+        setMaxPagesToShow(3);
+      } else {
+        setMaxPagesToShow(5);
+      }
+    };
+
+    handleResize();
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   if (currentPage <= half) {
     endPage = Math.min(totalPages, maxPagesToShow);
