8113ecc7a7fb7d91308d074113adf9edfff5433d
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-05 12:06:15 +0100

    Filter by type button section now uses drop down menu to display type buttons on mobile screen

--- src/components/FilterByTypeButtons.jsx
+++ src/components/FilterByTypeButtons.jsx
@@ -6,28 +6,44 @@ import { useState } from "react";
 export const FilterByTypeButtons = ({ filterByTypeFunc, activeFilter }) => {
   const myObj = typeColors;
 
-  return (
-    <div className="filterButtonContainer">
-      {Object.keys(myObj).map((key) => {
-        const isActive = activeFilter === key;
-        const iconUrl = getTypeIcon(key);
+  const [showFilters, setShowFilters] = useState(false);
 
-        return (
-          <button
-            key={key}
-            className={isActive ? "active" : ""}
-            style={{
-              border: `2px solid ${myObj[key]}`,
-              backgroundColor: isActive ? myObj[key] : "white",
-              color: isActive ? "white" : "black",
-            }}
-            onClick={() => filterByTypeFunc(key)}
-          >
-            <span>{firstLetterUpperCase(key)}</span>
-            <img src={iconUrl} alt={`${key} icon`} />
-          </button>
-        );
-      })}
-    </div>
+  return (
+    <>
+      <button
+        className="filterToggleBtn"
+        onClick={() => setShowFilters(!showFilters)}
+      >
+        Filter by Type {showFilters ? "▲" : "▼"}
+      </button>
+
+      {showFilters && (
+        <div className="filterButtonContainer">
+          {Object.keys(myObj).map((key) => {
+            const isActive = activeFilter === key;
+            const iconUrl = getTypeIcon(key);
+
+            return (
+              <button
+                key={key}
+                className={isActive ? "active" : ""}
+                style={{
+                  border: `2px solid ${myObj[key]}`,
+                  backgroundColor: isActive ? myObj[key] : "white",
+                  color: isActive ? "white" : "black",
+                }}
+                onClick={() => {
+                  filterByTypeFunc(key);
+                  setShowFilters(false);
+                }}
+              >
+                <span>{firstLetterUpperCase(key)}</span>
+                <img src={iconUrl} alt={`${key} icon`} />
+              </button>
+            );
+          })}
+        </div>
+      )}
+    </>
   );
 };
