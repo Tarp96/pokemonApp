855775555d2ab0eaa06f4ac0c2023a97b4590399
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-06 19:59:42 +0100

    Updated header JSX and styling to merge profile and log out/in buttons into one image button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -35,24 +35,49 @@ export const Header = () => {
         <NavLink to="/" className="headerNavBtn">
           Home
         </NavLink>
+
         <NavLink to="/game" className="headerNavBtn">
           Game
         </NavLink>
 
-        {userLoggedIn && (
-          <NavLink to="/profilepage" className="headerNavBtn">
-            Profile
-          </NavLink>
-        )}
-
-        {userLoggedIn ? (
-          <button onClick={logout} className="headerNavBtn">
-            Log Out
-          </button>
-        ) : (
+        {!userLoggedIn && (
           <NavLink to="/login" className="headerNavBtn">
             Log In
           </NavLink>
+        )}
+
+        {userLoggedIn && (
+          <div className="profileMenuWrapper">
+            <button className="profileAvatarBtn" onClick={toggleMenu}>
+              <img
+                src="https://boxchatter.wordpress.com/wp-content/uploads/2013/06/pkmn-trainer-red.jpg"
+                alt="Profile"
+                className="profileAvatarImg"
+              />
+            </button>
+
+            {menuOpen && (
+              <div className="profileDropdown">
+                <NavLink
+                  to="/profilepage"
+                  className="dropdownItem"
+                  onClick={() => setMenuOpen(false)}
+                >
+                  My Profile
+                </NavLink>
+
+                <button
+                  className="dropdownItem"
+                  onClick={() => {
+                    logout();
+                    setMenuOpen(false);
+                  }}
+                >
+                  Log Out
+                </button>
+              </div>
+            )}
+          </div>
         )}
       </div>
     </header>
