f428c19a7f16be521ae24fc3fbb294f7d37e5998
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-07 13:15:59 +0100

    Created listenToHighscore and updateHighscore functions
    Fixed type in import

--- src/services/highScoreService.js
+++ src/services/highScoreService.js
@@ -1,5 +1,6 @@
+import { onSnapshot } from "firebase/firestore";
 import { db } from "../firebaseConfig";
-import { doc, getDoc, updateDoc } from "firebase/firstore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 export const getUserHighScore = async (uid) => {
   const userRef = doc(db, "users", uid);
@@ -8,4 +9,31 @@ export const getUserHighScore = async (uid) => {
   if (!snapShot.exists()) return 0;
 
   return snapShot.data().highScore ?? 0;
+};
+
+export const listenToHighScore = (uid, callback) => {
+  const userRef = doc(db, "users", uid);
+
+  return onSnapshot(userRef, (docSnap) => {
+    if (docSnap.exists()) {
+      callback(docSnap.data().highScore ?? 0);
+    }
+  });
+};
+
+export const updateHighScore = async (uid, score) => {
+  const useRef = doc(db, "users", uid);
+
+  const snapShot = await getDoc(useRef);
+  const currentHighScore = snapshotEqual.data()?.highScore ?? 0;
+
+  if (currentHighScore >= score) {
+    throw new Error("Not enough to beat the current highscore!");
+  }
+
+  await updateDoc(useRef, {
+    highScore: score,
+  });
+
+  console.log("New highscore: ", score);
 };
