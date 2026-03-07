bfcd2880360ed2ebe21083ac6880549d203de229
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-07 12:57:32 +0100

    Created getUserHighScore function to retrieve users highscore for comparison

--- src/services/highScoreService.js
+++ src/services/highScoreService.js
@@ -1,2 +1,11 @@
 import { db } from "../firebaseConfig";
 import { doc, getDoc, updateDoc } from "firebase/firstore";
+
+export const getUserHighScore = async (uid) => {
+  const userRef = doc(db, "users", uid);
+  const snapShot = await getDoc(userRef);
+
+  if (!snapShot.exists()) return 0;
+
+  return snapShot.data().highScore ?? 0;
+};
