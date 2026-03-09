5824744b44e7eb33895308d7ca8eb5789dc7fa69
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-09 17:19:20 +0100

    feat(LeaderboardScreen): Leadboardscreen now displays leaderboard from firestore

--- src/pages/CatchGame/LeaderboardScreen.jsx
+++ src/pages/CatchGame/LeaderboardScreen.jsx
@@ -1,3 +1,32 @@
 import { getLeaderboardTop10 } from "../../services/leaderboardService";
 import { useState, useEffect } from "react";
-export const LeaderboardScreen = () => {};
+
+export const LeaderboardScreen = () => {
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    const fetchLeaderboard = async () => {
+      const arr = await getLeaderboardTop10();
+      setLeaderboard(arr);
+      console.log(arr);
+    };
+
+    fetchLeaderboard();
+  }, []);
+
+  const displayLeaderboard = leaderboard.map((item, index) => {
+    return (
+      <li key={index}>
+        {item.score} {item.username}
+      </li>
+    );
+  });
+
+  return (
+    <>
+      <div className="leaderboardPageContainer">
+        <ol>{displayLeaderboard}</ol>
+      </div>
+    </>
+  );
+};
