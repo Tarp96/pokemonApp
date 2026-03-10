cd7b3fdff9001c5ed5690255bca8e862d190046b
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-10 13:09:31 +0100

    Imported profile data hook to Gamestartscreen

--- src/pages/CatchGame/GameStartScreen.jsx
+++ src/pages/CatchGame/GameStartScreen.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import { useEffect, useState } from "react";
+import { useProfileData } from "../../hooks/useProfileData";
 
 export const GameStartScreen = ({
   selectedDifficulty,
