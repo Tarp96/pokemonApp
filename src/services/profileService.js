e08cbe5f87743a2819de8082d09eaec11f908772
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-11 15:38:13 +0100

    feat(profile): create profileService for user profile Firestore operations

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,2 +1,23 @@
 import { db } from "../firebaseConfig";
 import { doc, updateDoc, getDoc } from "firebase/firestore";
+
+export const getUserProfile = async (uid) => {
+  const ref = doc(db, "users", uid);
+  const snap = await getDoc(ref);
+
+  if (!snap.exists()) {
+    throw new Error("User not found");
+  }
+
+  return snap.data();
+};
+
+export const updateUserAvatar = async (uid, avatarId) => {
+  const ref = doc(db, "users", uid);
+  await updateDoc(ref, { avatarId });
+};
+
+export const updateUserQuote = async (uid, quoteId) => {
+  const ref = doc(db, "users", uid);
+  await updateDoc(ref, { quoteId });
+};
