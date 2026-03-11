e43e90da07490f56ee82ad9b7610d184c285fb5a
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-11 12:40:30 +0100

    Added pokemonQuotes file to data folder.
    Added the selected quotes to pokemonQuotes array

---
+++ src/data/pokemonQuotes.js
@@ -0,0 +1,78 @@
+const pokemonQuotes = [
+  {
+    id: 1,
+    quote:
+      "Never give up! To do so is an insult to the Pokémon who fight for you!",
+  },
+  {
+    id: 2,
+    quote: "I lost because I didn't eat enough pizza.",
+  },
+  {
+    id: 3,
+    quote: "When two trainers' eyes meet, it's battle time!",
+  },
+  {
+    id: 4,
+    quote:
+      "The circumstances of one's birth are irrelevant; it's what you choose to do with the gift of life that determines who you are.",
+  },
+  {
+    id: 5,
+    quote: "Are you feeling the itch to fish?",
+  },
+  {
+    id: 6,
+    quote:
+      "You don’t just find your place in the world. You’ve got to make it!",
+  },
+  {
+    id: 7,
+    quote:
+      "I should have brought my Game Boy Advance so I wouldn't get bored in line.",
+  },
+  {
+    id: 8,
+    quote:
+      "Strong Pokémon. Weak Pokémon. That is only the selfish perception of people. Truly skilled trainers should try to win with their favorites.",
+  },
+  {
+    id: 9,
+    quote:
+      "How can I get you to understand my hobby? Which is... talking in a deep voice!",
+  },
+  {
+    id: 10,
+    quote:
+      "A Caterpie may change into a Butterfree, but the heart that beats inside remains the same.",
+  },
+  {
+    id: 11,
+    quote:
+      "Did you pick out your clothes in the dark? Or perhaps from a trash can?",
+  },
+  {
+    id: 12,
+    quote:
+      "Even if we don't understand each other, that is not a reason to reject one another. There are two sides to every argument. Is there a side that has the answer to everything? Give it some thought!",
+  },
+  {
+    id: 13,
+    quote:
+      "I've made it this far in life using the karate I learned on the internet!",
+  },
+  {
+    id: 14,
+    quote: "We’ll always be connected even if we’re not in the same place.",
+  },
+  {
+    id: 15,
+    quote: "Lalalala… Lalalala… I am a loner…",
+  },
+  {
+    id: 16,
+    quote: "I will use my frying pan as a drying pan!",
+  },
+];
+
+export default pokemonQuotes;
