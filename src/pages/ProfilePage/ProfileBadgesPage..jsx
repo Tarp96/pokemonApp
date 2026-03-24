30573c942b751e1039acc5173d283a680a8a6487
Tarpi96 <tarpisingh.96@gmail.com>
2026-03-24 16:27:40 +0100

    feat(ProfileBadges):
    - Profile Badges page now displays all 8 badges and applies conditional styling based on if badge is unlocked or not
    
    - Applied styling

---
+++ src/pages/ProfilePage/ProfileBadgesPage..jsx
@@ -0,0 +1,17 @@
+import { useOutletContext } from "react-router-dom";
+import { getUnlockedBadges } from "../../utils/BADGES";
+import { BadgeCard } from "../../components/BadgeCard";
+
+export const ProfileBadgesPage = () => {
+  const { pokemonCaught } = useOutletContext();
+
+  const badges = getUnlockedBadges(pokemonCaught ?? 0);
+
+  return (
+    <div className="badgeGrid">
+      {badges.map((badge, index) => (
+        <BadgeCard key={index} badge={badge} index={index} />
+      ))}
+    </div>
+  );
+};
