056a9b0a46d95c4c0b8db3328b96588b2fff5a3f
Tarpi96 <tarpisingh.96@gmail.com>
2026-04-19 14:52:00 +0200

    style: Fixed styling for "NoPokemonMatchFilter" component to make it responsive

--- src/components/pokemon/NoPokemonMatchFilter.jsx
+++ src/components/pokemon/NoPokemonMatchFilter.jsx
@@ -2,17 +2,18 @@ export const NoPokemonMatchFilter = ({ onClick, type }) => {
   const message =
     type === "type"
       ? "No Pokémon found with the selected type. 🧬\nTry another type!"
-      : "No Pokémon found matching that name. 🧭\nCheck the spelling and try again!";
+      : "No Pokémon found matching that name. \nCheck the spelling and try again!";
 
   return (
     <div className="ifNoPokemonMatchFilter">
       <p>{message}</p>
-      <button onClick={onClick}>Remove Filter</button>
+
       <img
         src="assets/pikaconfused.gif"
         alt="yellow mouse looking dizzy"
         className="ifNoPokemonInFilterImage"
       />
+      <button onClick={onClick}>Remove Filter</button>
     </div>
   );
 };
