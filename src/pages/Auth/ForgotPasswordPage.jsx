a1da82fad66d40eaaa40aebed5c2df6e56492a01
Tarpi96 <tarpisingh.96@gmail.com>
2026-05-09 16:30:45 +0200

    feat(ForgotPasswordPage):
    - Added form and input button
    
    - Styled form and input button

--- src/pages/Auth/ForgotPasswordPage.jsx
+++ src/pages/Auth/ForgotPasswordPage.jsx
@@ -1,6 +1,9 @@
 import loginArt from "../../assets/loginPic.webp";
+import { useState } from "react";
 
 export const ForgotPasswordPage = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <>
       <div className="forgotPasswordPageContainer">
@@ -9,6 +12,18 @@ export const ForgotPasswordPage = () => {
         </div>
         <div className="forgotPasswordFormContainer">
           <h1>Forgot your password?</h1>
+
+          <form className="forgotPasswordForm">
+            <input
+              type="email"
+              placeholder="Enter your email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+
+            <button type="submit">Send Reset Email</button>
+          </form>
         </div>
       </div>
     </>
