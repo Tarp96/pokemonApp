142ac739e2cffd7179d6a31d4bb73a73caf7cc02
Tarpi96 <tarpisingh.96@gmail.com>
2026-05-12 15:39:21 +0200

    refactor(App): Wrapped routes in AnimatePresence for smoother transitions when navigating

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import "./styles/App.css";
 import { HomePage } from "./pages/HomePage/HomePage";
 import { PokemonDetailsPage } from "./pages/PokemonDetailsPage/PokemonDetailsPage";
@@ -26,38 +26,47 @@ import { NotFoundPage } from "./pages/NotFoundPage";
 import { ForgotPasswordPage } from "./pages/Auth/ForgotPasswordPage";
 
 function App() {
+  const location = useLocation();
+
   return (
     <>
-      <Routes>
-        <Route path="*" element={<NotFoundPage />} />
-        <Route path="/" element={<HomePageLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="profilepage" element={<ProfilePageOverview />}>
-            <Route index element={<ProfilePage />} />
-            <Route path="favorites" element={<FavoritePokemonsPage />} />
-            <Route path="team" element={<ProfileTeamPage />} />
-            <Route path="badges" element={<ProfileBadgesPage />} />
-          </Route>
-          <Route path="login" element={<AuthLayout />}>
-            <Route index element={<LoginPage />} />
-            <Route path="register" element={<RegisterUserPage />} />
-            <Route path="forgot-password" element={<ForgotPasswordPage />} />
-          </Route>
-          <Route path="game" element={<GamePageLayout />}>
-            <Route index element={<GamePage />} />
-            <Route path="leaderboard" element={<LeaderboardScreen />} />
-          </Route>
-          <Route path="trainer/:userId" element={<TrainerCardPage />} />
-        </Route>
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="*" element={<NotFoundPage />} />
 
-        <Route path="/pokemon/:name" element={<PokemonDetailsPage />}>
-          <Route index element={<PokemonDetailOverView />} />
-          <Route path="stats" element={<PokemonStats />} />
-          <Route path="photos" element={<PokemonPhotos />} />
-          <Route path="games" element={<PokemonGames />} />
-          <Route path="moves" element={<PokemonMoves />} />
-        </Route>
-      </Routes>
+          <Route path="/" element={<HomePageLayout />}>
+            <Route index element={<HomePage />} />
+
+            <Route path="profilepage" element={<ProfilePageOverview />}>
+              <Route index element={<ProfilePage />} />
+              <Route path="favorites" element={<FavoritePokemonsPage />} />
+              <Route path="team" element={<ProfileTeamPage />} />
+              <Route path="badges" element={<ProfileBadgesPage />} />
+            </Route>
+
+            <Route path="login" element={<AuthLayout />}>
+              <Route index element={<LoginPage />} />
+              <Route path="register" element={<RegisterUserPage />} />
+              <Route path="forgot-password" element={<ForgotPasswordPage />} />
+            </Route>
+
+            <Route path="game" element={<GamePageLayout />}>
+              <Route index element={<GamePage />} />
+              <Route path="leaderboard" element={<LeaderboardScreen />} />
+            </Route>
+
+            <Route path="trainer/:userId" element={<TrainerCardPage />} />
+          </Route>
+
+          <Route path="/pokemon/:name" element={<PokemonDetailsPage />}>
+            <Route index element={<PokemonDetailOverView />} />
+            <Route path="stats" element={<PokemonStats />} />
+            <Route path="photos" element={<PokemonPhotos />} />
+            <Route path="games" element={<PokemonGames />} />
+            <Route path="moves" element={<PokemonMoves />} />
+          </Route>
+        </Routes>
+      </AnimatePresence>
 
       <ToastContainer
         position="top-center"
