df3fabcb3520095914facc535ad00db7b165c928
Tarpi96 <tarpisingh.96@gmail.com>
2026-05-12 15:52:30 +0200

    feat(PageTransition): Created component to handle page transitions, to avoid repeating animation code everywhere

---
+++ src/components/animations/PageTransition.jsx
@@ -0,0 +1,35 @@
+import { motion } from "framer-motion";
+
+const pageVariants = {
+  initial: {
+    opacity: 0,
+    y: 15,
+  },
+  animate: {
+    opacity: 1,
+    y: 0,
+  },
+  exit: {
+    opacity: 0,
+    y: -15,
+  },
+};
+
+function PageTransition({ children }) {
+  return (
+    <motion.div
+      variants={pageVariants}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      transition={{
+        duration: 0.25,
+        ease: "easeInOut",
+      }}
+    >
+      {children}
+    </motion.div>
+  );
+}
+
+export default PageTransition;
